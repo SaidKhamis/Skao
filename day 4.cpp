6a13bf472822f24642a1e7648b2e289290a9b520
SaidKhamis <saidkhamis2306@gmail.com>
2024-12-27 23:05:09 +0300

    Function prototypes
    line of codes to describe the function prototypes for our project.

---
+++ day 4.cpp
@@ -0,0 +1,5 @@
+//function prototypes
+void addTask(vector<Task>& tasks);
+void viewTasks(const vector<Task>& tasks);
+void markcompletedTask(vector<Task>& task);
+void deleteTask(vector<Task>& task);
