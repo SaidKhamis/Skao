0d15ac8e00305b13ff4331ac1324963a6a72aff6
SaidKhamis <saidkhamis2306@gmail.com>
2024-12-31 09:47:28 +0300

    confirmation message
    -Storing the added task with its attribute values.
    -after ading the attributes values of the task then the confirmation message will be display "task added successsfully".

---
+++ day 21.cpp
@@ -0,0 +1,3 @@
+	task.push_back(newTask);			//Storing the added task and provide feedbak below.
+	cout << "    Task Added Successfully! \n";	task.push_back(newTask);			//Storing the added task and provide feedbak below.
+	cout << "    Task Added Successfully! \n";
